f58a76e78043a0a3ab7837cf48c5d5e468415f42
Jeff Hui & Kurtis Seebaldt <pair+jeff+kurtis@pivotallabs.com>
2012-09-07 17:43:58 -0700

    Converted pwd to local

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -268,7 +268,7 @@ ERROR
 
      pipe('find #{bin_dir}')
 
-     run("bundle config build.sqlite3 --with-sqlite3-dir=`pwd`/vendor/sqlite --with-sqlite3-include=`pwd`/vendor/sqlite/include --with-sqlite3-lib=`pwd`/vendor/sqlite/lib")
+     run("bundle config build.sqlite3 --with-sqlite3-dir=vendor/sqlite --with-sqlite3-include=vendor/sqlite/include --with-sqlite3-lib=vendor/sqlite/lib")
   end
 
   # default set of binaries to install
